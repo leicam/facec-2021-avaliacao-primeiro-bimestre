f50f471a3dedf313dcf6bd1391f0abc65bbd54d6
Juliano Ribeiro de Souza Maciel <juliano.maciel@totvs.com.br>
2021-07-06 22:25:58 -0300

    Correção de exercicios

---
+++ Prova.Apresentacao/Cliente.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova.Apresentacao
+{
+    public class Cliente
+    {
+        public string Documento { get; private set; }
+        public string Nome { get; private set; }
+
+        public Cliente(string documento, string nome)
+        {
+            Documento = documento;
+            Nome = nome;
+
+            ValidarDocumento();
+            ValidarNome();
+        }
+
+        private void ValidarNome()
+        {
+            if (string.IsNullOrEmpty(Nome))
+                throw new ArgumentOutOfRangeException(nameof(Nome));
+        }
+
+        private void ValidarDocumento()
+        {
+            if (string.IsNullOrEmpty(Documento) || (Documento.Length != 11 && Documento.Length != 14))
+                throw new ArgumentOutOfRangeException(nameof(Documento));
+        }
+
+        public override string ToString()
+        {
+            return $"{Documento} - {Nome}";
+        }
+    }
+}
---
+++ Prova.Testes/ClienteTestes.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prova.Apresentacao;
+using System;
+
+namespace Prova.Testes
+{
+    [TestClass]
+    public class ClienteTestes
+    {
+        private const string _owner = "Juliano";
+        private const string _category = "Cliente";
+
+        [TestMethod]
+        [Owner(_owner)]
+        [TestCategory(_category)]
+        public void Cliente_Construtor_NomeExpectedException()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => new Cliente("11111111111", string.Empty),
+                "Não foi gerada exception esperada para a propriedade Nome.");
+        }
+
+        [TestMethod]
+        [Owner(_owner)]
+        [TestCategory(_category)]
+        public void Cliente_Construtor_DocumentoExpectedException()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(
+                () => new Cliente(string.Empty, "Teste"),
+                "Não foi gerada exception esperada para a propriedade Documento.");
+        }
+
+        [TestMethod]
+        [Owner(_owner)]
+        [TestCategory(_category)]
+        public void Cliente_ToString_ExpectedSucesso()
+        {
+            var cliente = new Cliente("11111111111", "Teste");
+
+            Assert.AreEqual("11111111111 - Teste", cliente.ToString(),
+                "Não foi gerado corretamente a string.");
+        }
+    }
+}
